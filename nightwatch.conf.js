c106c80513a9db2647cf78c9eeaf4c6ccd930ad6
AbinayaPalani <abinaya.palani@a-cti.com>
2019-02-13 13:43:28 +0530

    Nightwatch configuration file

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,81 +1,102 @@
-const seleniumServer = require('selenium-server');
-const chromedriver = require('chromedriver');
-//const nightwatchCucumber = require('nightwatch-cucumber');
+const seleniumServer = require('selenium-server-standalone-jar');
+const chromeDriver = require('chromedriver');
+const SCREENSHOT_PATH = "./screenshots/";
 
-
-
-// var nightwatchCucumberConf = {
-//   runner: 'nightwatch',
-//   featureFiles: './tests/features/support/AccountDetail.feature',
-//   stepDefinitions: './tests/features/step_definitions/steps.js',
-//   closeSession: 'afterFeature'
-// }
-
-
-
-require('nightwatch-cucumber')({
-  cucumberArgs: ['--require', './tests/admintool/CucumberHook/hooks.js','--require', './tests/admintool/step_definitions/.','--format', 'node_modules/cucumber-pretty', '--format', 'json:reports/cucumber.json', '--format-options', '{"colorsEnabled":false}', './tests/admintool/features/AccountDetail.feature']
-});
+// const geckoDriver = require('geckodriver');
+// const ieDriver = require('iedriver');
+// const edgeDriver = require('edgedriver');
 
 module.exports = {
-  output_folder: 'reports',
-  custom_assertions_path: '',
-  page_objects_path: 'page_objects',
-  live_output: false,
-  disable_colors: false,
+  src_folders: ["./step-definitions"],
+  output_folder: './reports',
+  custom_commands_path: '',
+  page_objects_path: '',
   selenium: {
     start_process: true,
     server_path: seleniumServer.path,
-    log_path: 'log/',
     host: '127.0.0.1',
-    port: 4444
+    port: 8080,
+    cli_args: {
+      "webdriver.chrome.driver" : chromeDriver.path,
+      //"webdriver.gecko.driver" : geckoDriver.path
+      //"webdriver.ie.driver" : ieDriver.path,
+      //"webdriver.edge.driver" : edgeDriver.path
+    }
   },
-  // test_workers:{
-  //   enabled : true,
-  //   workers : "auto"
-  // },
   test_settings: {
-    end_session_on_fail : false,
-    skip_testcases_on_fail : false,
-
     default: {
-      launch_url: 'http://localhost',
-      selenium_port: 4444,
-      selenium_host: '127.0.0.1',
-      end_session_on_fail : false,
-    skip_testcases_on_fail : false,
-    screenshots : {
-          enabled : true,
-          on_failure : true,
-          on_error : true,
-          path : "reports/screenshots"
-    },
-
       desiredCapabilities: {
         browserName: 'chrome',
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
-          args: ['incognito', '--disable-infobars', 'start-fullscreen']
+          args: ['disable-infobars','start-fullscreen','disable-notifications']
         }
       },
-      selenium: {
-        cli_args: {
-          'webdriver.chrome.driver': chromedriver.path
+        screenshots: {
+          enabled: true, // if you want to keep screenshots
+          on_failure: true,
+          on_error:false,
+          path: './screenshots' // save screenshots here
+        },
+        globals: {
+          waitForConditionTimeout: 50000 // sometimes internet is slow so wait.
         }
-      }
     },
     chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
         javascriptEnabled: true,
-        acceptSslCerts: true
-      },
-      selenium: {
-        cli_args: {
-          'webdriver.chrome.driver': chromedriver.path
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['disable-infobars']
         }
       }
-    } 
+    // },
+    // firefox: {
+    //   desiredCapabilities: {
+    //     browserName: 'firefox',
+    //     javascriptEnabled: true,
+    //     acceptSslCerts: true,
+    //     marionette: true
+    //   }
+    // },
+    // edge: {
+    //   desiredCapabilities: {
+    //     browserName: 'MicrosoftEdge',
+    //     javascriptEnabled: true,
+    //   }
+    // },
+    // ie: {
+    //   desiredCapabilities: {
+    //     browserName: 'internet explorer',
+    //     javascriptEnabled: true,
+    //   }
+    }
   }
 };
+
+function padLeft (count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
+  return count < 10 ? '0' + count : count.toString();
+}
+
+var FILECOUNT = 0; // "global" screenshot file count
+/**
+ * The default is to save screenshots to the root of your project even though
+ * there is a screenshots path in the config object above! ... so we need a
+ * function that returns the correct path for storing our screenshots.
+ * While we're at it, we are adding some meta-data to the filename, specifically
+ * the Platform/Browser where the test was run and the test (file) name.
+ */
+function imgpath (browser) {
+  var a = browser.options.desiredCapabilities;
+  var meta = [a.platform];
+  meta.push(a.browserName ? a.browserName : 'any');
+  meta.push(a.version ? a.version : 'any');
+  meta.push(a.name); // this is the test filename so always exists.
+  var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
+  return SCREENSHOT_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
+}
+
+module.exports.imgpath = imgpath;
+module.exports.SCREENSHOT_PATH = SCREENSHOT_PATH;
